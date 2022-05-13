6d7e81c2e56e191f040c7a0f416ac1e4f82a6ddc
Dragon17 <olenik27042005@gmail.com>
2022-05-13 20:19:47 +0300

    bacc

--- src/Components/RegisterAdmin/index.jsx
+++ src/Components/RegisterAdmin/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import {DataBaseLink} from '../../Host.js'
 
 import './RegisterAdmin.scss';
 
